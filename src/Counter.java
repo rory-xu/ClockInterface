80248177e3559281ada94575ec24e3dd7a5bf8a8
Rockorory <electronicwiz01@gmail.com>
2022-03-02 14:30:28 -0500

    a counter interface in the form of clock display that supports increments of one in hours, minutes, or seconds

---
+++ src/Counter.java
@@ -0,0 +1,117 @@
+/**
+ * Counter class that displays an adjustable timer in standard or military time
+ */
+
+public class Counter{
+	public static final int SECONDS_PER_HOUR = 3600;
+	public static final int SECONDS_PER_MINUTE = 60;
+	public static final int SECONDS_PER_DAY = 24 * 60 * 60;
+	private int totalSeconds;
+
+	/**
+	 * Initializes counter at desired time
+	 * @param hour Initialized hour
+	 * @param minute Initialized minute
+	 * @param second Initialized second
+	 */
+	public Counter(int hour, int minute, int second) {
+		totalSeconds = hour * 3600 + minute * 60 + second;
+	}
+
+	/**
+	 * Initializes counter at 00:00:00 military or 12:00:00 for standard
+	 */
+	public Counter() {
+		totalSeconds = 0;
+	}
+
+	public int getTotalSeconds() {
+		return totalSeconds;
+	}
+
+	/**
+	 * Increments hour counter by one
+	 */
+	public void incrementHour() {
+		addSeconds(SECONDS_PER_HOUR);
+	}
+
+	/**
+	 * Decrements hour counter by one
+	 */
+	public void decrementHour() {
+		addSeconds(-SECONDS_PER_HOUR);
+	}
+
+	/**
+	 * Increments minute counter by one
+	 */
+	public void incrementMinute() {
+		addSeconds(SECONDS_PER_MINUTE);
+	}
+
+	/**
+	 * Decrements minute counter by one
+	 */
+	public void decrementMinute() {
+		addSeconds(-SECONDS_PER_MINUTE);
+	}
+
+	/**
+	 * Increments second counter by one
+	 */
+	public void incrementSecond() {
+		addSeconds(1);
+	}
+
+	/**
+	 * Decrements second counter by one
+	 */
+	public void decrementSecond() {
+		addSeconds(-1);
+	}
+
+	/**
+	 * Displays the counter in military time
+	 * @return A string showing the counter in military time
+	 */
+	public String displayMilitary() {
+		return display(totalSeconds / SECONDS_PER_HOUR);
+	}
+
+	/**
+	 * Displays the counter in standard time
+	 * @return A string showing the counter in standard time
+	 */
+	public String displayStandard() {
+		return display((totalSeconds / SECONDS_PER_HOUR) % 12 == 0 ? 12 : (totalSeconds / SECONDS_PER_HOUR) % 12);
+	}
+
+	/**
+	 * Adds an integer amount of seconds to the counter
+	 * Compatible with both positive and negative numbers (for increment decrement)
+	 * @param seconds Number of seconds to be added
+	 */
+	private void addSeconds(int seconds) {
+		totalSeconds += seconds;
+		if (totalSeconds < 0) totalSeconds += SECONDS_PER_DAY;
+		else if (totalSeconds >= SECONDS_PER_DAY) totalSeconds -= SECONDS_PER_DAY;
+	}
+
+	/**
+	 * Displays the counter in ##:##:## format
+	 * @param hour The format of the hour counter depending on standard or military display
+	 * @return A string showing the counter in ##:##:## format
+	 */
+	private String display(int hour) {
+		return String.format("%02d:%02d:%02d", hour,
+				totalSeconds % SECONDS_PER_HOUR / SECONDS_PER_MINUTE,
+				totalSeconds % SECONDS_PER_MINUTE);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Counter)) return false;
+		return (totalSeconds == ((Counter) obj).getTotalSeconds());
+	}
+}
---
+++ tests/CounterTest.java
@@ -0,0 +1,173 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CounterTest {
+
+	@Test
+	void hours_increments_properly() {
+		Counter check = new Counter(13, 30, 30);
+		Counter testCounter = new Counter(12, 30, 30);
+		testCounter.incrementHour();
+
+		assertEquals(check, testCounter);
+	}
+
+	@Test
+	void minutes_increments_properly() {
+		Counter check = new Counter(4, 31, 30);
+		Counter testCounter = new Counter(4, 30, 30);
+		testCounter.incrementMinute();
+
+		assertEquals(check.getTotalSeconds(), testCounter.getTotalSeconds());
+	}
+
+	@Test
+	void seconds_increments_properly() {
+		Counter check = new Counter(4, 30, 31);
+		Counter testCounter = new Counter(4, 30, 30);
+		testCounter.incrementSecond();
+
+		assertEquals(check, testCounter);
+	}
+
+	@Test
+	void hours_decrements_properly() {
+		Counter check = new Counter(4, 30, 30);
+		Counter testCounter = new Counter(5, 30, 30);
+
+		testCounter.decrementHour();
+
+		assertEquals(check, testCounter);
+	}
+
+	@Test
+	void minutes_decrements_properly() {
+		Counter check = new Counter(4, 30, 30);
+		Counter testCounter = new Counter(4, 31, 30);
+		testCounter.decrementMinute();
+
+		assertEquals(check.getTotalSeconds(), testCounter.getTotalSeconds());
+	}
+
+	@Test
+	void seconds_decrements_properly() {
+		Counter check = new Counter(4, 30, 30);
+		Counter testCounter = new Counter(4, 30, 31);
+		testCounter.decrementSecond();
+
+		assertEquals(check, testCounter);
+	}
+
+	@Test
+	void hours_increments_properly_at_23_hours() {
+		Counter check = new Counter(0, 30, 30);
+		Counter testCounter = new Counter(23, 30, 30);
+		testCounter.incrementHour();
+
+		assertEquals(check, testCounter);
+	}
+
+	@Test
+	void minutes_increments_properly_at_59_minutes() {
+		Counter check = new Counter(5, 00, 30);
+		Counter testCounter = new Counter(4, 59, 30);
+		testCounter.incrementMinute();
+
+		assertEquals(check.getTotalSeconds(), testCounter.getTotalSeconds());
+	}
+
+	@Test
+	void seconds_increments_properly_at_59_seconds() {
+		Counter check = new Counter(4, 31, 00);
+		Counter testCounter = new Counter(4, 30, 59);
+		testCounter.incrementSecond();
+
+		assertEquals(check, testCounter);
+	}
+
+	@Test
+	void hours_decrements_properly_at_00_hours() {
+		Counter check = new Counter(23, 30, 30);
+		Counter testCounter = new Counter(0, 30, 30);
+
+		testCounter.decrementHour();
+
+		assertEquals(check, testCounter);
+	}
+
+	@Test
+	void minutes_decrements_properly_at_00_minutes() {
+		Counter check = new Counter(4, 59, 30);
+		Counter testCounter = new Counter(5, 00, 30);
+		testCounter.decrementMinute();
+
+		assertEquals(check.getTotalSeconds(), testCounter.getTotalSeconds());
+	}
+
+	@Test
+	void seconds_decrements_properly_at_00_seconds() {
+		Counter check = new Counter(4, 30, 59);
+		Counter testCounter = new Counter(4, 31, 00);
+		testCounter.decrementSecond();
+
+		assertEquals(check, testCounter);
+	}
+
+	@Test
+	void carry_over_chains_properly_at_plus_one_second_23_59_59() {
+		Counter check = new Counter(00, 00, 00);
+		Counter testCounter = new Counter(23, 59, 59);
+		testCounter.incrementSecond();
+
+		assertEquals(check, testCounter);
+	}
+
+	@Test
+	void carry_over_chains_properly_at_minus_one_second_00_00_00() {
+		Counter check = new Counter(23, 59, 59);
+		Counter testCounter = new Counter(00, 00, 00);
+		testCounter.decrementSecond();
+
+		assertEquals(check, testCounter);
+	}
+
+	@Test
+	void displays_military_time_properly() {
+		Counter testCounter = new Counter(4, 30, 30);
+		testCounter.displayMilitary();
+
+		assertEquals("04:30:30", testCounter.displayMilitary());
+	}
+
+	@Test
+	void displays_standard_time_properly() {
+		Counter testCounter = new Counter(4, 30, 30);
+		testCounter.displayStandard();
+
+		assertEquals("04:30:30", testCounter.displayStandard());
+	}
+
+	@Test
+	void displays_military_time_properly_after_12_hours() {
+		Counter testCounter = new Counter(13, 30, 30);
+		testCounter.displayMilitary();
+
+		assertEquals("13:30:30", testCounter.displayMilitary());
+	}
+
+	@Test
+	void displays_standard_time_properly_after_12_hours() {
+		Counter testCounter = new Counter(13, 30, 30);
+		testCounter.displayStandard();
+
+		assertEquals("01:30:30", testCounter.displayStandard());
+	}
+
+	@Test
+	void displays_12_at_00_hours_standard() {
+		Counter testCounter = new Counter(00, 30, 30);
+		testCounter.displayStandard();
+
+		assertEquals("12:30:30", testCounter.displayStandard());
+	}
+}
